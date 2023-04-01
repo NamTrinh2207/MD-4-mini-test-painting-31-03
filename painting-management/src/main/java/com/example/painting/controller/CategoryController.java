fc3becb8bd44e2156bbd90492a01ab50572a44f5
NamTrinh2207 <trinhnam180697@gmail.com>
2023-04-01 19:52:12 +0700

    update

--- painting-management/src/main/java/com/example/painting/controller/CategoryController.java
+++ painting-management/src/main/java/com/example/painting/controller/CategoryController.java
@@ -1,4 +1,24 @@
 package com.example.painting.controller;
 
+import com.example.painting.model.Category;
+import com.example.painting.service.category.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/categories")
 public class CategoryController {
+    @Autowired
+    private ICategoryService categoryRepo;
+    @GetMapping("")
+    public ModelAndView list() {
+        Iterable<Category> categories = categoryRepo.findAll();
+        ModelAndView modelAndView = new ModelAndView("/category/homes");
+        modelAndView.addObject("categories", categories);
+        return modelAndView;
+    }
 }
